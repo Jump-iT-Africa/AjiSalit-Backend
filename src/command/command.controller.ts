b7423fe5534a6069bdb17822c94c0d9f029f2ad3
SalBouhamidi <bouhamidi.sal@gmail.com>
2025-06-11 17:46:10 +0100

    Changing CronoExpression to test reminder

--- src/command/command.controller.ts
+++ src/command/command.controller.ts
@@ -1162,7 +1162,7 @@ export class CommandController {
     summary:
       "The reminder Notification sent to the client to after the order is done to get his order",
   })
-  @Cron(CronExpression.EVERY_DAY_AT_3PM)
+  @Cron(CronExpression.EVERY_5_MINUTES)
   async clientReminderNorification() {
     try {
       return await this.commandService.commandClientReminder();
@@ -1175,7 +1175,7 @@ export class CommandController {
     summary:
       "The reminder Notification sent to the company in case the company does not deliver after the deadline",
   })
-  @Cron(CronExpression.EVERY_DAY_AT_9AM)
+  @Cron(CronExpression.EVERY_5_MINUTES)
   async companyReminderNotification() {
     try {
       return await this.commandService.commandCompanyReminder();
