c75393e14d50d0f69b5e116246de3f2c167bbf28
SalBouhamidi <bouhamidi.sal@gmail.com>
2025-06-20 16:18:46 +0100

    switching to server side

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ async function bootstrap() {
   .setDescription('AjiSalit is an app that help you track your orders and get them on time')
   .setVersion('1.0')
   // .addServer('http://localhost:3000/', 'Local environment')
+  
   .addServer('https://api.ajisalit.com/', 'Production')
 
   .addBearerAuth()
